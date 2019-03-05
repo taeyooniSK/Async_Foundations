475c1dd608b2e6678a7c854c19d5a93c9411c9ed
Tae-yoon <yeotaeyoon@gmail.com>
2019-03-05 22:52:46 +0900

    Chain Promises

--- AsyncFoundations.js
+++ AsyncFoundations.js
@@ -49,16 +49,50 @@
     Promise Chaining: Returning Data
 */
 
-const promise = new Promise((resolve, reject) => {
-    resolve(3);
-})
+// const promise = new Promise((resolve, reject) => {
+//     resolve(3);
+// })
 
-promise.then(data => {
-    return data * 2;
+// promise.then(data => {
+//     return data * 2;
+// })
+// .then( data => {
+//     return data + 10;
+// })
+// .then( data => {
+//     console.log(data);
+// })
+
+
+/* 
+    Chain Promises
+*/
+
+let counter = 0;
+function increaseCounter(){
+    counter++;
+    console.log(`Counter: ${counter}`);
+}
+
+function runLater(callback, timeInMs){
+    const p = new Promise((resolve, reject) => {
+        setTimeout( () => {
+            // const response = callback();
+            // resolve(response);
+            callback();
+            resolve(callback);
+        }, timeInMs);
+    })
+    return p;
+}
+
+runLater(increaseCounter, 1000)
+.then( () => {
+    return runLater(increaseCounter, 2000);
 })
-.then( data => {
-    return data + 10;
+.then( () => {
+    return runLater(increaseCounter, 3000);
 })
-.then( data => {
-    console.log(data);
+.then(counter => {
+    console.log(`Promise is done`);
 })
